4945a0dc2288cef81d0b95d62ef42d7e85e0e4a0
kayamayoki <k32142004@gmail.com>
2021-06-24 18:42:05 +0900

    practice

--- lesson1.py
+++ lesson1.py
@@ -11,3 +11,23 @@ print("kayama" + "yoki" ) # renketu
 
 name = "kayama"  
 print("My name is" + name)
+
+
+string_price = "1000"
+price = 500
+
+total_price = int(string_price) + price # use int
+print(total_price)
+
+li =[]
+li.append("python")
+print(li)
+li.append("php")
+print(li)
+
+
+profile = {"name":"yoki","email":"yoki@gmail.com"}
+print(profile["name"])
+
+profile["gender"] = "male"
+print(profile)
